75759a71ab42086d088f113d78526effb4f085dc
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-16 04:07:33 -0600

    Add interval variable

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,19 @@
 const sentence = "hello there from lighthouse labs";
 
 const type = function(input) {
-  let x = 0;
+  let delay = 0;
+  const interval = 50;
   for (const char of input) {
     setTimeout(() => {
       process.stdout.write(char);
-    }, x);
-    x += 50;
+    }, delay);
+    delay += interval;
   }
 
   //prints space as soon as sentence is done typing
   setTimeout(() => {
     console.log();
-  }, (sentence.length * 50));
+  }, (sentence.length * interval));
 
 };
 
